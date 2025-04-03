cee34617053f374e46bab76f2c4715049e49baec
kushagra vyas <kushagravyas1122@gmail.com>
2025-04-03 18:44:30 +0530

    function

--- Day1.js
+++ Day1.js
@@ -23,3 +23,37 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 // console.log(1 + "2" + "3");
 
 
+console.log("2" == 2); // Here the answer is true because JS convert the string datatype in integer by itself.
+
+console.log("2" === 2); // Here the ansewer is false because in this case we are using Strict Check
+//  by using (===) so here we want the same data type because JS does not convert data type
+//  by itself in Strict Check.
+
+
+// # Data Type
+
+//  1. Primitive Datatype => 7 datatypes : String, Number, Bigint, Boolean, Null, Undifined, Symbol.
+    
+//  2. Reference (Non primitive datatype) : Arrays, Objects, Functions.  
+
+ const name = ["Kushagra", "Nidhi", "Vyas"]  // This is called Array
+
+    let myObj = {
+    name: "kushagra", 
+    age: 22,                  // this is called object
+ }
+
+
+ const myfunction = function(a){
+    console.log("kushagra vyas", a);
+ }
+
+
+ myfunction(1)
+   
+ 
+                
+ 
+
+
+       
