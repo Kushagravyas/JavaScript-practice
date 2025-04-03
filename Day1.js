b9e9af149e8dc49d0267b082be53cc0ff38823e2
kushagra vyas <kushagravyas1122@gmail.com>
2025-04-03 17:00:37 +0530

    type conversion

---
+++ Day1.js
@@ -0,0 +1,14 @@
+const age = null
+
+//console.log(typeof age);
+//console.log(typeof(age));
+
+//console.log(typeof 22);
+let valueInNumber = Number(age);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
