b9d1e4650c46fd3934ff6487153a4e5ea23a4a0c
kushagra vyas <kushagravyas1122@gmail.com>
2025-05-17 16:17:46 +0530

    try catch block

---
+++ Error_handling.js
@@ -0,0 +1,14 @@
+try {
+    console("Hello Kushagra")
+}
+catch (err) {
+    console.log("Plz,check your code", err.message)
+
+}
+finally {
+    console.log("Hello Vyas")
+}
+
+// err:{
+//     message: "console is not defined",
+// }
