84e1d971864196129113bc74173750a2499ee625
kushagra vyas <kushagravyas1122@gmail.com>
2025-05-17 16:56:09 +0530

    custome error

--- Error_handling.js
+++ Error_handling.js
@@ -12,3 +12,20 @@ finally {
 // err:{
 //     message: "console is not defined",
 // }
+
+function dividenumbers(a, b){
+    try {
+        if (b === 0) {
+            throw new Error("you can not divide by zero")
+        }
+        let result = a/b;
+        console.log("Result", result);
+    } catch(error) {
+           console.log(error.message)
+    } finally {
+        console.log("Division has been executed")
+    }
+}
+
+dividenumbers(6,2);
+dividenumbers(3,0);
