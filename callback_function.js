aa7e7eeb5f3280ab5b05a433a9e7c9f4657909a9
kushagra vyas <kushagravyas1122@gmail.com>
2025-05-17 20:50:07 +0530

    functions created

---
+++ callback_function.js
@@ -0,0 +1,41 @@
+function eatdinner(callback) {
+    console.log("Dinner is cooking");
+    
+    setTimeout(() => {
+        console.log("Dinner is getting ready");
+        callback();
+    },3000)
+}
+
+function ready() {
+    console.log("I'm eating dinner");
+}
+
+eatdinner(ready);
+
+
+
+
+function travel(wait) {
+    console.log("I have booked ticket")
+    
+    setTimeout(() => {
+        console.log("I'm coming by bus")
+        wait();
+    }, 2000)
+}
+
+function reached() {
+    console.log("I have reached")
+}
+
+travel(reached);
+
+const fruits = ["apple", "banana", "cherry"];
+
+fruits.forEach(function(fruit) {
+  console.log("I like", fruit);
+});
+
+
+
