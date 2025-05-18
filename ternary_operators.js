5121b9147072317dabb88daa877bfe1458603a08
kushagra vyas <kushagravyas1122@gmail.com>
2025-05-18 22:36:55 +0530

    learning operators

---
+++ ternary_operators.js
@@ -0,0 +1,25 @@
+// Ternary operators are oneline if else statements
+    // we can write twrnary operators like that (condition ?  valueIftrue : valueIffalse;)
+
+
+    // Example
+
+    let isHungry = true;
+
+    let message = isHungry ? "yes I am " : "not right now";
+    console.log(message);
+
+
+
+    let isWaiting = false;
+
+    let reply = isWaiting ? "Yes" : "No"
+    console.log(reply);
+
+
+    function greetUser(isLoggedIn) {
+  return isLoggedIn ? "Welcome back!" : "Please log in";
+}
+
+console.log(greetUser(true));
+// console.log(greetUser(false));
