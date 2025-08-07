8b8ea90034ffa15cda91fba29c1488585536f011
kushagra vyas <kushagravyas1122@gmail.com>
2025-08-08 04:02:59 +0530

    new questions

--- today.js
+++ today.js
@@ -28,3 +28,19 @@ const deepCopy = JSON.parse(JSON.stringify(original));
 deepCopy.skills.backend = 'Express';
 
 console.log(original.skills.backend);  // Output: 'Node.js' ✅ original is safe
+
+
+function oddEven(n) {
+  for(let i = 1; i <= n; i++){
+    if(i % 2 === 0){
+      console.log(i + " is even");
+    } else {
+      console.log(i + " is odd");
+    }
+  }
+}
+oddEven(3);
+
+function oddEven1() {
+  let result 
+}
