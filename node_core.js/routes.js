4ab7af686b834ee58876ac409b24524320a812e7
kushagra vyas <kushagravyas1122@gmail.com>
2025-08-13 02:42:40 +0530

    new program added

--- node_core.js/routes.js
+++ node_core.js/routes.js
@@ -33,3 +33,38 @@ app.get('/user/:id', (req, res) => {
 
 app.listen (3000, () => console.log("Server is listening on port 3000"));
 
+
+const express = require("express");
+const ap = express();
+
+app.get('/', (req, res) => {
+    res.send("welcome to home page!");
+});
+
+app.post('/submit', (req, res) => {
+    res.send("User Submited");
+});
+
+app.get('/user/:id', (req, res) => {
+    res.send(`User ID: ${req.params.id}`);
+});
+
+app.listen(3000, () => console.log("Server is listening on port 3000"));
+
+
+const express = require("express");
+const appss = express();
+
+app.get('/', (req, res) => {
+    res.send("Welcome to homepage!");
+});
+
+app.post('/user', (req, res) => {
+    res.send("User submitted!");
+});
+
+app.get('/user/:id', (req, res) => {
+    res.send(`User ID: ${req.params.id}`);
+});
+
+app.listen(3000, () => console.log("Server is listening on PORT 3000"))
