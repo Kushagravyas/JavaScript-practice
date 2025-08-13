ed48295bc4e94936e4e8353cb57b8386bcf6fd33
kushagra vyas <kushagravyas1122@gmail.com>
2025-08-13 18:42:38 +0530

    practice callbacks

--- callback_function.js
+++ callback_function.js
@@ -1,41 +1,41 @@
-// function eatdinner(callback) {
-//     console.log("Dinner is cooking");
+function eatdinner(callback) {
+    console.log("Dinner is cooking");
     
-//     setTimeout(() => {
-//         console.log("Dinner is getting ready");
-//         callback();
-//     },3000)
-// }
+    setTimeout(() => {
+        console.log("Dinner is getting ready");
+        callback();
+    },3000)
+}
 
-// function ready() {
-//     console.log("I'm eating dinner");
-// }
+function ready() {
+    console.log("I'm eating dinner");
+}
 
-// eatdinner(ready);
-
-
+eatdinner(ready);
 
 
-// function travel(wait) {
-//     console.log("I have booked ticket")
+
+
+function travel(wait) {
+    console.log("I have booked ticket")
     
-//     setTimeout(() => {
-//         console.log("I'm coming by bus")
-//         wait();
-//     }, 2000)
-// }
+    setTimeout(() => {
+        console.log("I'm coming by bus")
+        wait();
+    }, 2000)
+}
 
-// function reached() {
-//     console.log("I have reached")
-// }
+function reached() {
+    console.log("I have reached")
+}
 
-// travel(reached);
+travel(reached);
 
-// const fruits = ["apple", "banana", "cherry"];
+const fruits = ["apple", "banana", "cherry"];
 
-// fruits.forEach(function(fruit) {
-//   console.log("I like", fruit);
-// });
+fruits.forEach(function(fruit) {
+  console.log("I like", fruit);
+});
 
 
 
