fe8b4d38994177e47125e04f7018cc8713ce52c1
kushagra vyas <kushagravyas1122@gmail.com>
2025-08-13 18:41:03 +0530

    Synchronous Callback

--- callback_function.js
+++ callback_function.js
@@ -1,42 +1,61 @@
-function eatdinner(callback) {
-    console.log("Dinner is cooking");
+// function eatdinner(callback) {
+//     console.log("Dinner is cooking");
     
-    setTimeout(() => {
-        console.log("Dinner is getting ready");
-        callback();
-    },3000)
-}
+//     setTimeout(() => {
+//         console.log("Dinner is getting ready");
+//         callback();
+//     },3000)
+// }
 
-function ready() {
-    console.log("I'm eating dinner");
-}
+// function ready() {
+//     console.log("I'm eating dinner");
+// }
 
-eatdinner(ready);
-
-
+// eatdinner(ready);
 
 
-function travel(wait) {
-    console.log("I have booked ticket")
+
+
+// function travel(wait) {
+//     console.log("I have booked ticket")
     
-    setTimeout(() => {
-        console.log("I'm coming by bus")
-        wait();
-    }, 2000)
+//     setTimeout(() => {
+//         console.log("I'm coming by bus")
+//         wait();
+//     }, 2000)
+// }
+
+// function reached() {
+//     console.log("I have reached")
+// }
+
+// travel(reached);
+
+// const fruits = ["apple", "banana", "cherry"];
+
+// fruits.forEach(function(fruit) {
+//   console.log("I like", fruit);
+// });
+
+
+
+// synchronous callback.
+
+function greet(name) {
+    console.log(`Hello, ${name}!`);
 }
 
-function reached() {
-    console.log("I have reached")
+function greetUser(callback) {
+    let name = "Kushagra Vyas";
+    callback(name);
 }
+greetUser(greet);
 
-travel(reached);
-
-const fruits = ["apple", "banana", "cherry"];
-
-fruits.forEach(function(fruit) {
-  console.log("I like", fruit);
-});
-
-
-
-
+function get(name) {
+    console.log(`Hello, ${name}`);
+}
+function getUser(callback) {
+    let name = "kushagra";
+    callback(name);
+}
+getUser(get);
