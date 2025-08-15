a19cbfd891c11d2b10d410328fb64f731660532f
kushagra vyas <kushagravyas1122@gmail.com>
2025-08-15 18:11:05 +0530

    file updated with new program

---
+++ UseEffect.js
@@ -0,0 +1,33 @@
+import React, {useEffect , useState} from "react";
+
+function Example() {
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        console.log("component rerendered or count changed : ", count)
+    }, [count]);
+
+    return(
+        <div>
+            <h2> count : {count} </h2>
+            <button onClick={() => setCount(count + 1)}>Increment</button>
+        </div>
+    );
+};
+export default Example; 
+
+function Counter() {
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        console.log("Component rerender or count chamged : ", {count});
+    }, [count]);
+
+    return(
+        <div>
+            <h2> count : {count} </h2>
+            <button onClick={() => setCount(count + 1)}>Increment</button>
+        </div>
+    );
+};
+
